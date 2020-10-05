e6a4cf0a5f57d3a79212863850ddb7f4cd27b883
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2020-10-05 17:20:27 +0530

    Add files via upload

---
+++ 2020/october/1/05-Oct/classes and objects(6).py
@@ -0,0 +1,13 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+p1 = person("John",36)
+
+p1.age = 40
+
+print(p1.age)
