7206eaf1e3519a33b911483f80961c423fb35334
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2020-10-11 10:16:02 +0530

    Add files via upload

---
+++ 2020/october/1/11-Oct/mysql(21).py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "myusername",
+    password = "mypassword",
+    database = "mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE IF EXISTS customers"
+
+mycursor.execute(sql)
++
