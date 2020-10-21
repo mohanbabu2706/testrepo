968155de6f5de539bf49683f3f8761dffad90d41
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2020-10-21 09:59:10 +0530

    Add files via upload

---
+++ 2020/october/1/20-Oct/dictonaries,generator expressions.py
@@ -0,0 +1,7 @@
+price = {'apple': 0.40,'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(price[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print('I owe the grocer $%.2f' % grocery_bill)
